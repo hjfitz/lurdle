202999ebe668b4aaec76ef54ceabbdbd5325091e
Harry Fitzgerald <harry.fitzgerald@truepill.com>
2022-03-28 11:47:15 +0100

    dammit prettier + better skip progress

--- src/guess.state.ts
+++ src/guess.state.ts
@@ -1,53 +1,58 @@
 export class GameState {
-	readonly maxGuesses: 5 = 5
-	#guessState: number = 0
-	#progressState: number = 0
+  readonly maxGuesses: 5 = 5;
+  readonly #maxSeconds: 10 = 10;
+  #guessState: number = 0;
+  #progressState: number = 0;
 
-	nextState(): boolean {
-		this.#guessState = this.#guessState + 1
-		return this.hasLost()
-	}
+  nextState(): boolean {
+    this.#guessState = this.#guessState + 1;
+    return this.hasLost();
+  }
 
-	getState(): number {
-		return this.#guessState
-	}
+  getState(): number {
+    return this.#guessState;
+  }
 
-	hasLost(): boolean {
-		return this.#guessState >= this.maxGuesses
-	}
+  getSkipPerc(): number {
+    console.log(this.getSeconds(), this.#maxSeconds);
+    return Math.floor((this.getSeconds() / this.#maxSeconds) * 100);
+  }
 
-	public nextProgressState(): boolean {
-		this.#progressState += 1
-		return this.nextState()
-	}
+  hasLost(): boolean {
+    return this.#guessState >= this.maxGuesses;
+  }
 
-	public getProgressState(): number {
-		return this.#progressState
-	}
+  public nextProgressState(): boolean {
+    this.#progressState += 1;
+    return this.nextState();
+  }
 
-	getSeconds(): number {
-		// 1 second 0, 1 total guesses
-		// 2 seconds for 2
-		// 3 seconds for 3
-		// 5 seconds for 5 guesses
-		switch (this.#guessState) {
-			case 0:
-			case 1: {
-				return 2
-			}
-			case 2: {
-				return 5
-			}
-			case 3: {
-				return 8
-			}
-			default: {
-				return 10
-			}
-		}
-	}
+  public getProgressState(): number {
+    return this.#progressState;
+  }
 
+  getSeconds(): number {
+    // 1 second 0, 1 total guesses
+    // 2 seconds for 2
+    // 3 seconds for 3
+    // 5 seconds for 5 guesses
+    switch (this.#guessState) {
+      case 0:
+      case 1: {
+        return 2;
+      }
+      case 2: {
+        return 5;
+      }
+      case 3: {
+        return 8;
+      }
+      default: {
+        return this.#maxSeconds;
+      }
+    }
+  }
 }
 
-// singleton 
-export default new GameState()
+// singleton
+export default new GameState();
