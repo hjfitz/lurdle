66b2ebff37ce1dc76adc3a559892cde461661778
Harry Fitzgerald <hjfitz@pm.me>
2022-03-28 21:33:30 +0100

    eslint init

--- src/guess.state.ts
+++ src/guess.state.ts
@@ -1,58 +1,57 @@
 export class GameState {
-  readonly maxGuesses: 5 = 5;
-  readonly #maxSeconds: 10 = 10;
-  #guessState: number = 0;
-  #progressState: number = 0;
+	readonly maxGuesses: 5 = 5
+	readonly #maxSeconds: 10 = 10
+	#guessState = 0
+	#progressState = 0
 
-  nextState(): boolean {
-    this.#guessState = this.#guessState + 1;
-    return this.hasLost();
-  }
+	public nextState(): boolean {
+		this.#guessState = this.#guessState + 1
+		return this.hasLost()
+	}
 
-  getState(): number {
-    return this.#guessState;
-  }
+	public getState(): number {
+		return this.#guessState
+	}
 
-  getSkipPerc(): number {
-    console.log(this.getSeconds(), this.#maxSeconds);
-    return Math.floor((this.getSeconds() / this.#maxSeconds) * 100);
-  }
+	public getSkipPerc(): number {
+		return Math.floor((this.getSeconds() / this.#maxSeconds) * 100)
+	}
 
-  hasLost(): boolean {
-    return this.#guessState >= this.maxGuesses;
-  }
+	public hasLost(): boolean {
+		return this.#guessState >= this.maxGuesses
+	}
 
-  public nextProgressState(): boolean {
-    this.#progressState += 1;
-    return this.nextState();
-  }
+	public nextProgressState(): boolean {
+		this.#progressState += 1
+		return this.nextState()
+	}
 
-  public getProgressState(): number {
-    return this.#progressState;
-  }
+	public getProgressState(): number {
+		return this.#progressState
+	}
 
-  getSeconds(): number {
-    // 1 second 0, 1 total guesses
-    // 2 seconds for 2
-    // 3 seconds for 3
-    // 5 seconds for 5 guesses
-    switch (this.#guessState) {
-      case 0:
-      case 1: {
-        return 2;
-      }
-      case 2: {
-        return 5;
-      }
-      case 3: {
-        return 8;
-      }
-      default: {
-        return this.#maxSeconds;
-      }
-    }
-  }
+	public getSeconds(): number {
+		// 1 second 0, 1 total guesses
+		// 2 seconds for 2
+		// 3 seconds for 3
+		// 5 seconds for 5 guesses
+		switch (this.#guessState) {
+		case 0:
+		case 1: {
+			return 2
+		}
+		case 2: {
+			return 5
+		}
+		case 3: {
+			return 8
+		}
+		default: {
+			return this.#maxSeconds
+		}
+		}
+	}
 }
 
 // singleton
-export default new GameState();
+export default new GameState()
--- src/main.ts
+++ src/main.ts
@@ -1,169 +1,173 @@
-import { Events } from "./db.js";
-import guessState from "./guess.state.js";
-import { VideoState } from "./video.state.js";
+import { Events } from './db.js'
+import guessState from './guess.state.js'
+import { VideoState } from './video.state.js'
 
 class Game {
-  readonly #guessState = guessState;
-  readonly #videoState: VideoState;
-  // elements
-  readonly #guessButton: HTMLAnchorElement;
-  readonly #playButton: HTMLAnchorElement;
-  readonly #options: HTMLSelectElement;
-  readonly #output: HTMLElement;
-  readonly #progressBar: HTMLDivElement;
-  readonly #skipButton: HTMLAnchorElement;
+	readonly #guessState = guessState
+	readonly #videoState: VideoState
+	// elements
+	readonly #guessButton: HTMLAnchorElement
+	readonly #playButton: HTMLAnchorElement
+	readonly #options: HTMLSelectElement
+	readonly #output: HTMLDialogElement
+	readonly #progressBar: HTMLDivElement
+	readonly #skipButton: HTMLAnchorElement
 
-  constructor(
-    guessButton: HTMLAnchorElement,
-    videoElement: HTMLDivElement,
-    playButton: HTMLAnchorElement,
-    options: HTMLSelectElement,
-    output: HTMLElement,
-    progress: HTMLDivElement,
-    skip: HTMLAnchorElement
-  ) {
-    // elements
-    // todo: normalise button naming convention
-    this.#guessButton = guessButton;
-    this.#playButton = playButton;
-    this.#skipButton = skip;
-    this.#options = options;
-    this.#output = output;
-    this.#progressBar = progress;
+	constructor(
+		guessButton: HTMLAnchorElement,
+		videoElement: HTMLDivElement,
+		playButton: HTMLAnchorElement,
+		options: HTMLSelectElement,
+		output: HTMLDialogElement,
+		progress: HTMLDivElement,
+		skip: HTMLAnchorElement
+	) {
+		// elements
+		// todo: normalise button naming convention
+		this.#guessButton = guessButton
+		this.#playButton = playButton
+		this.#skipButton = skip
+		this.#options = options
+		this.#output = output
+		this.#progressBar = progress
 
-    // game bits
-    this.addEventListeners();
-    this.initialiseDropdownOptions();
-    this.initialiseProgressMarkers();
+		// game bits
+		this.addEventListeners()
+		this.initialiseDropdownOptions()
+		this.initialiseProgressMarkers()
 
-    // misc
-    this.#videoState = new VideoState(this.#guessState, videoElement);
-  }
+		// misc
+		this.#videoState = new VideoState(this.#guessState, videoElement)
+	}
 
-  initialiseProgressMarkers() {}
+	initialiseProgressMarkers() {}
 
-  initialiseDropdownOptions() {
-    Object.entries(Events)
-      .sort(() => Math.random() - 0.5)
-      .forEach(([id, opt]) => {
-        const el = document.createElement("option");
-        el.textContent = opt;
-        el.dataset.eventId = id;
-        this.#options.appendChild(el);
-      });
-  }
+	initialiseDropdownOptions() {
+		Object.entries(Events)
+			.sort(() => Math.random() - 0.5)
+			.forEach(([id, opt]) => {
+				const el = document.createElement('option')
+				el.textContent = opt
+				el.dataset.eventId = id
+				this.#options.appendChild(el)
+			})
+	}
 
-  addEventListeners() {
-    this.#guessButton.addEventListener("click", this.tryGuess.bind(this));
-    this.#skipButton.addEventListener("click", this.moveSkipState.bind(this));
-    this.#playButton.addEventListener("click", () =>
-      this.#videoState.playForGuess()
-    );
-    console.log("added event listeners");
-  }
+	addEventListeners() {
+		this.#guessButton.addEventListener('click', this.tryGuess.bind(this))
+		this.#skipButton.addEventListener('click', this.moveSkipState.bind(this))
+		this.#playButton.addEventListener('click', () => this.#videoState.playForGuess())
+		document.addEventListener('click', (ev) => {
+			const target = ev?.target as Element
+			if (target === this.#output) return
+			// @ts-expect-error spec bad
+			this.#output.open = false
+		})
+	}
 
-  moveSkipState(ev: MouseEvent) {
-    console.log("updating skip state");
-    ev.preventDefault();
-    if (this.#guessState.hasLost()) return;
-    const hasLost = this.#guessState.nextProgressState();
-    // move progress bar
-    const progressBar = this.#progressBar.querySelector(
-      "#guess-progress"
-    )! as HTMLDivElement;
-    const percProgress = this.#guessState.getSkipPerc();
-    progressBar.style.width = `${percProgress}%`;
-    if (hasLost) {
-      progressBar.classList.add("bg-gray-900");
-      progressBar.classList.remove("bg-gray-100");
-      this.#skipButton.classList.add("disabled");
-      this.writeOutput(false, this.#guessState.maxGuesses);
-    }
-  }
+	moveSkipState(ev: MouseEvent) {
+		ev.preventDefault()
+		if (this.#guessState.hasLost()) return
+		const hasLost = this.#guessState.nextProgressState()
+		// move progress bar
+		const progressBar = this.#progressBar.querySelector(
+			'#guess-progress'
+		)! as HTMLDivElement
+		const percProgress = this.#guessState.getSkipPerc()
+		progressBar.style.width = `${percProgress}%`
+		if (hasLost) {
+			progressBar.classList.add('bg-gray-900')
+			progressBar.classList.remove('bg-gray-100')
+			this.#skipButton.classList.add('disabled')
+			this.writeOutput(false, this.#guessState.maxGuesses)
+		}
+	}
 
-  getGuess(): string {
-    return this.#options.value;
-  }
+	getGuess(): string {
+		return this.#options.value
+	}
 
-  tryGuess(ev: MouseEvent) {
-    ev.preventDefault();
+	tryGuess(ev: MouseEvent) {
+		ev.preventDefault()
 
-    // move to next guess state
-    this.#guessState.nextState();
-    const guess = this.getGuess();
-    const isMatch = this.#videoState.getVideo() === guess;
-    const totalGuesses = this.#guessState.getState();
-    this.writeProgress(totalGuesses);
-    this.writeOutput(isMatch, totalGuesses);
-  }
+		// move to next guess state
+		this.#guessState.nextState()
+		const guess = this.getGuess()
+		const isMatch = this.#videoState.getVideo() === guess
+		const totalGuesses = this.#guessState.getState()
+		this.writeProgress(totalGuesses)
+		this.writeOutput(isMatch, totalGuesses)
+	}
 
-  writeProgress(totalGuesses: number) {}
+	writeProgress(totalGuesses: number) {}
 
-  async writeOutput(matches: boolean, guesses: number) {
-    console.log({ guesses });
-    this.#output.innerHTML = "";
-    const p = document.createElement("p");
-    const node = document.createTextNode("🧙🏻‍♂️");
-    p.appendChild(node);
+	async writeOutput(matches: boolean, guesses: number) {
+		console.log({ guesses })
+		this.#output.innerHTML = ''
+		const p = document.createElement('p')
+		const node = document.createTextNode('🧙🏻‍♂️')
+		p.appendChild(node)
 
-    // todo: look in to splitting skipstate and gamestate
-    Array.from({ length: guesses }, (_, idx) => {
-      let node = document.createTextNode("⬛️");
-      if (matches && idx + 1 === guesses) {
-        node = document.createTextNode("🟩");
-      }
-      p.appendChild(node);
-    });
+		// todo: look in to splitting skipstate and gamestate
+		Array.from({ length: guesses }, (_, idx) => {
+			let node = document.createTextNode('⬛️')
+			if (matches && idx + 1 === guesses) {
+				node = document.createTextNode('🟩')
+			}
+			p.appendChild(node)
+		})
 
-    const text = document.createElement("p");
-    text.textContent = "#Lurdle ⚔️";
-    if (matches || this.#guessState.hasLost()) {
-      this.#output.appendChild(text);
-      this.#output.appendChild(document.createTextNode("\n\n"));
-      this.#output.appendChild(p);
-    }
+		const text = document.createElement('p')
+		text.textContent = '#Lurdle ⚔️'
+		if (matches || this.#guessState.hasLost()) {
+			this.#output.appendChild(text)
+			this.#output.appendChild(document.createTextNode('\n\n'))
+			this.#output.appendChild(p)
+			// @ts-expect-error dialog not fully supported
+			this.#output.open = true
+			//await navigator.clipboard.writeText(this.#output.textContent!);
+		}
 
-    await navigator.clipboard.writeText(this.#output.textContent!);
-  }
+	}
 }
 
 function getElementStrict<T = HTMLElement>(
-  id: string,
-  root: Document = document
+	id: string,
+	root: Document = document
 ) {
-  const elem = root.getElementById(id);
-  if (!elem) {
-    throw new Error("Unable to find element: " + id);
-  }
-  return elem as unknown as T;
+	const elem = root.getElementById(id)
+	if (!elem) {
+		throw new Error('Unable to find element: ' + id)
+	}
+	return elem as unknown as T
 }
 
 // @ts-expect-error external lib
 window.onYouTubeIframeAPIReady = function () {
-  // buttons
-  const btnGuess = getElementStrict<HTMLAnchorElement>("btn-guess");
-  const btnSkip = getElementStrict<HTMLAnchorElement>("btn-skip");
-  const btnPlay = getElementStrict<HTMLAnchorElement>("btn-play");
+	// buttons
+	const btnGuess = getElementStrict<HTMLAnchorElement>('btn-guess')
+	const btnSkip = getElementStrict<HTMLAnchorElement>('btn-skip')
+	const btnPlay = getElementStrict<HTMLAnchorElement>('btn-play')
 
-  // youtube api root
-  const vidPlayer = getElementStrict<HTMLDivElement>("video-root");
+	// youtube api root
+	const vidPlayer = getElementStrict<HTMLDivElement>('video-root')
 
-  // where you pick your scene
-  const opts = getElementStrict<HTMLSelectElement>("game-options");
+	// where you pick your scene
+	const opts = getElementStrict<HTMLSelectElement>('game-options')
 
-  // should be a modal
-  const out = getElementStrict("output");
-  const progress = getElementStrict<HTMLDivElement>("game-progress");
+	// should be a modal
+	const out = getElementStrict<HTMLDialogElement>('output')
+	const progress = getElementStrict<HTMLDivElement>('game-progress')
 
-  // initialise the game
-  const game = new Game(
-    btnGuess,
-    vidPlayer,
-    btnPlay,
-    opts,
-    out,
-    progress,
-    btnSkip
-  );
-  console.log(game);
-};
+	// initialise the game
+	const game = new Game(
+		btnGuess,
+		vidPlayer,
+		btnPlay,
+		opts,
+		out,
+		progress,
+		btnSkip
+	)
+	console.log(game)
+}
